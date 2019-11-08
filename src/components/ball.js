12329383d6c9dc8122d5b1b5a2dd26b1f14dee83
DANIEL BRAUER <danielbrauer@DANIELs-MacBook-Pro.local>
2019-11-08 09:34:21 -0500

    moving state from ball; interaction of ball + paddle happens up in game class

--- src/components/ball.js
+++ src/components/ball.js
@@ -3,76 +3,6 @@ import { Circle } from "react-konva";
 
 export default class Ball extends PureComponent {
 
-    state = {
-        x: 0,
-        y: 0,
-        direction: { x: 0, y: 0 }
-    };
-
-    paddleHit = (
-        ballX, ballY, deltaY,
-        paddleLeft, paddleRight, paddleTop, paddleBottom
-    ) => {
-        let newDeltaY = deltaY;
-        if (ballX >= paddleLeft && ballX <= paddleRight && ballY >= paddleBottom && ballY <= paddleTop ) {
-            newDeltaY = -deltaY;
-        }
-        return newDeltaY;
-    };
-
-    newCoord = (val, delta, max, min) => {
-        let newVal = val + delta;
-        let newDelta = delta;
-
-        if (newVal > max || newVal < min) {
-            newDelta = -delta;
-        }
-
-        if (newVal < min) {
-            newVal = min - newVal;
-        }
-        if (newVal > max) {
-            newVal = newVal - (newVal - max);
-        }
-
-        return { val: newVal, delta: newDelta };
-    };
-
-    componentDidMount() {
-        const x = this.props.paddleX;
-        const y = this.props.paddleY;
-        const dirX = Math.floor(Math.random() * this.props.ballSpeed);
-        const dirY = this.props.ballSpeed - x;
-        this.setState({ x, y, direction: { x: dirX, y: dirY } });
-        this.animate();
-    }
-
-    animate = () => {
-        const { direction, x, y } = this.state;
-
-        if (direction.x !== 0 || direction.y !== 0) {
-            const newYDir = this.paddleHit(
-                x, y, direction.y,
-                this.props.paddleX, (this.props.paddleX + this.props.paddleWidth),
-                (this.props.paddleY + this.props.paddleHeight), this.props.paddleY
-            );
-
-            const newX = this.newCoord(x, direction.x, this.props.ballMaxX, this.props.ballMinY);
-            const newY = this.newCoord(y, newYDir, this.props.ballMaxY, this.props.ballMinY);
-
-            this.setState({
-                x: newX.val,
-                y: newY.val,
-                direction: {
-                    x: newX.delta,
-                    y: newY.delta
-                }
-            });
-        }
-
-        this.animationTimeout = setTimeout(this.animate, 50);
-    };
-
     renderBall = (x, y, color) => {
         return (
             <Circle
@@ -89,11 +19,7 @@ export default class Ball extends PureComponent {
     };
 
     render() {
-        const { x, y } = this.state;
-        return this.renderBall(x, y, this.props.ballColor );
+        return this.renderBall(this.props.ballXCoord, this.props.ballYCoord, this.props.ballColor );
     }
 
-    componentWillUnmount() {
-        clearTimeout(this.animationTimeout);
-    }
 }
--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Layer, Circle, Rect, Group} from "react-konva";
+import {Layer, Group} from "react-konva";
 import Field from "./field";
 import Ball from "./ball";
 import Paddle from "./paddle";
@@ -10,24 +10,34 @@ export default class Game extends Component {
 
     FIELD_WIDTH = 400;
     FIELD_HEIGHT = 400;
+    PADDLE_OFFSET = 50;
     BALL_MIN_X = 12;
     BALL_MIN_Y = 12;
     BALL_MAX_X = this.FIELD_WIDTH - this.BALL_MIN_X;
     BALL_MAX_Y = this.FIELD_HEIGHT - this.BALL_MIN_Y;
     BALL_COLOR = Konva.Util.getRandomColor();
 
+
     constructor(props) {
         super(props);
         this.state = {
             fieldWidth: this.FIELD_WIDTH,
             fieldHeight: this.FIELD_HEIGHT,
+
             paddleWidth: 60,
             paddleHeight: 10,
             paddleX: 0,
-            paddleY: 50,
-            paddleOffset: 50,
+            paddleY: this.FIELD_HEIGHT - this.PADDLE_OFFSET,
+
             ballSpeed: 15,
             ballRadius: 10,
+            ballDirection: {
+                x: 0,
+                y: 0
+            },
+            ballXCoord: 0,
+            ballYCoord: 0,
+
             brickWidth: 380,
             brickHeight: 10,
             bricks: [
@@ -38,15 +48,103 @@ export default class Game extends Component {
     };
 
     _onMouseMove = ({ evt }) => {
-        // console.log("event e.evt.clientX", evt.clientX);
         this.setState({
+            ...this.state,
             paddleX: evt.clientX
         });
     };
 
-    paddleCollision = () => {
+    //////////////// BALL ANIMATION START
 
+    paddleHit = (
+        ballX, ballY, deltaY,
+        paddleLeft, paddleRight, paddleTop, paddleBottom
+    ) => {
+        let newDeltaY = deltaY;
+        if (ballX >= paddleLeft && ballX <= paddleRight && ballY >= paddleBottom && ballY <= paddleTop ) {
+            newDeltaY = -deltaY;
+        }
+        return newDeltaY;
     };
+
+    newCoord = (val, delta, max, min) => {
+        let newVal = val + delta;
+        let newDelta = delta;
+
+        if (newVal > max || newVal < min) {
+            newDelta = -delta;
+        }
+
+        if (newVal < min) {
+            newVal = min - newVal;
+        }
+        if (newVal > max) {
+            newVal = newVal - (newVal - max);
+        }
+
+        return { val: newVal, delta: newDelta };
+    };
+
+    animateBall = () => {
+        const { ballDirection, ballXCoord, ballYCoord } = this.state;
+
+        if (ballDirection.x !== 0 || ballDirection.y !== 0) {
+            const newYDir = this.paddleHit(
+                ballXCoord, ballYCoord, ballDirection.y,
+                this.state.paddleX, (this.state.paddleX + this.state.paddleWidth),
+                (this.state.paddleY + this.state.paddleHeight), this.state.paddleY
+            );
+
+            const newX = this.newCoord(ballXCoord, ballDirection.x, this.BALL_MAX_X, this.BALL_MIN_X);
+            const newY = this.newCoord(ballYCoord, newYDir, this.BALL_MAX_Y, this.BALL_MIN_Y);
+
+            this.setState({
+                ...this.state,
+                ballDirection: {
+                    x: newX.delta,
+                    y: newY.delta
+                },
+                ballXCoord: newX.val,
+                ballYCoord: newY.val
+            });
+        }
+
+        this.animationTimeout = setTimeout(this.animateBall, 50);
+    };
+
+    //////////////// BALL ANIMATION END
+
+
+
+
+
+
+
+
+
+
+    componentDidMount() {
+        const x = this.state.paddleX;
+        const y = this.state.paddleY;
+        const dirX = Math.floor(Math.random() * this.state.ballSpeed);
+        const dirY = this.state.ballSpeed - x;
+
+        this.setState({
+            ...this.state,
+            ballDirection: {
+                x: dirX,
+                y: dirY
+            },
+            ballXCoord: x,
+            ballYCoord: y,
+        });
+
+        this.animateBall();
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.animationTimeout);
+    }
 
 
 
@@ -70,23 +168,16 @@ export default class Game extends Component {
                     fieldHeight={this.state.fieldHeight}
                 />
                 <Ball
-                    ballSpeed={this.state.ballSpeed}
-                    ballRadius={this.state.ballRadius}
-                    paddleX={this.state.paddleX}
-                    paddleY={this.state.fieldHeight - this.state.paddleOffset}
-                    paddleWidth={this.state.paddleWidth}
-                    paddleHeight={this.state.paddleHeight}
-                    ballMinX={this.BALL_MIN_X}
-                    ballMinY={this.BALL_MIN_Y}
-                    ballMaxX={this.BALL_MAX_X}
-                    ballMaxY={this.BALL_MAX_Y}
                     ballColor={this.BALL_COLOR}
+                    ballRadius={this.state.ballRadius}
+                    ballXCoord={this.state.ballXCoord}
+                    ballYCoord={this.state.ballYCoord}
                 />
                 <Paddle
                     fieldWidth={this.state.fieldWidth}
                     fieldHeight={this.state.fieldHeight}
                     paddleX={this.state.paddleX}
-                    paddleY={this.state.fieldHeight - this.state.paddleOffset}
+                    paddleY={this.state.paddleY}
                     paddleWidth={this.state.paddleWidth}
                     paddleHeight={this.state.paddleHeight}
                 />
